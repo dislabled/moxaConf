7e04b00b8981b1fe4a3126c29b04121e364f6e5b
Stian Knudsen <dislabled@gmail.com>
2023-11-08 15:15:07 +0100

    some frames ready

--- new_main.py
+++ new_main.py
@@ -23,59 +23,82 @@ class MainApp(tk.Tk):
         self.minsize(size[0], size[1])
         self.bind("<Escape>", lambda _: self.destroy())
 
-        # Create the widgets.
-        # self.Menu = Menu(self)
-        # self.Main = Main(self)
-        self.AutoConfMenu = AutoConfMenu(self)
-        self.AutoConf = AutoConf(self)
+        # Create some "globals".
+        self.shared_vals: dict = {
+            "toggle_conf": tk.IntVar(value=0),
+            "toggle_mainred": tk.IntVar(value=0),
+            "logged_in": tk.BooleanVar(value=False),
+            "active_side_frame": tk.IntVar(value=0),
+            "active_main_frame": tk.IntVar(value=0),
+        }
+        # Monitor the globals.
+        self.shared_vals["active_side_frame"].trace("w", self.menu_frame)
+        self.shared_vals["active_main_frame"].trace("w", self.main_frame)
+
+        # Open the frames.
+        self.menu_frame(self.shared_vals)
+        self.main_frame()
 
         # Create the main frame.
         self.mainloop()
 
+    def menu_frame(self, *args):
+        """Return the menu frame."""
+        _ = args  # Hush some editor warnings
+        menu_frame = ttk.Frame(self)
+        menu_frame.place(relx=0, rely=0, width=250, relheight=1)
+        if self.shared_vals["active_side_frame"].get() == 0:
+            MainMenu(menu_frame, self.shared_vals)
+        else:
+            AutoConfMenu(menu_frame, self.shared_vals)
 
-class Menu(ttk.Frame):
-    """Menu frame for moxa configurator."""
+    def main_frame(self, *args):
+        """Return the main frame."""
+        _ = args  # Hush some editor warnings
+        main_frame = ttk.Frame(self)
+        main_frame.place(relx=0.25, rely=0, relwidth=0.75, relheight=1)
+        if self.shared_vals["active_main_frame"].get() == 0:
+            pass
+        elif self.shared_vals["active_main_frame"].get() == 1 and self.shared_vals["logged_in"].get() is True:
+            SwitchInfo(main_frame)
+            Ports(main_frame)
+        elif self.shared_vals["active_main_frame"].get() == 2:
+            AutoConf(main_frame, self.shared_vals)
 
-    def __init__(self, parent):
+
+class MainMenu(ttk.Frame):
+    """Main menu buttons."""
+
+    def __init__(self, parent, shared_vals):
         """Initialize the class."""
         super().__init__(parent)
-        self.place(relx=0, rely=0, relwidth=0.25, relheight=1)
+        self.logged_in = shared_vals["logged_in"]
+        self.active_main_frame = shared_vals["active_main_frame"]
+        self.active_side_frame = shared_vals["active_side_frame"]
         self.create_widgets()
         self.create_layout()
 
     def create_widgets(self):
         """Create the widgets."""
         self.menuframe = ttk.LabelFrame(self, text="Menu")
-        self.button0 = tk.Button(self.menuframe, text="Connect", width=13)
+        self.button0 = tk.Button(self.menuframe, text="Connect", width=13, command=self.connect)
         self.button1 = tk.Button(self.menuframe, text="Download config", width=13, command=self.download_config)
         self.button2 = tk.Button(
             self.menuframe,
             text="Transfer firmware",
             width=13,
         )
-        # command=lambda: controller.show_frame(Firmware),)
-        self.button3 = tk.Button(
-            self.menuframe,
-            text="Auto configure",
-            width=13,
-        )
-        # command=lambda: controller.show_frame(AutoConf),)
+        self.button3 = tk.Button(self.menuframe, text="Auto configure", width=13, command=self.autoconfig)
         self.button4 = tk.Button(self.menuframe, text="Factory reset", width=13, command=self.factory_reset)
         self.button5 = tk.Button(
             self.menuframe,
             text="Alarm log",
             width=13,
         )
-        # command=lambda: controller.show_frame(LogView),)
         self.button6 = tk.Button(self.menuframe, text="Copy ram2rom", width=13, command=self.apply)
 
     def create_layout(self):
         """Create the layout."""
-        # Create the grid.
-        # self.columnconfigure((0), weight=1, uniform='a')
-        # self.rowconfigure((0,1,2,3,4,5,6,7), weight=1, uniform='a')
-
-        # Place the buttons.
         self.menuframe.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
         self.button0.grid(row=0, column=0, padx=2, pady=2)
         self.button1.grid(row=1, column=0, padx=2, pady=2)
@@ -84,6 +107,7 @@ class Menu(ttk.Frame):
         self.button4.grid(row=4, column=0, padx=2, pady=2)
         self.button5.grid(row=5, column=0, padx=2, pady=2)
         self.button6.grid(row=6, column=0, padx=2, pady=2)
+        self.pack()
 
     def factory_reset(self):
         """Reset switch to factory settings."""
@@ -112,17 +136,22 @@ class Menu(ttk.Frame):
         else:
             mb.showerror(title="Error", message="Something went wrong")
 
+    def connect(self):
+        """Connect to switch."""
+        self.config(cursor="watch")
+        login_mode = moxa_switch.check_login()
+        if login_mode == 0:
+            moxa_switch.menu_login()
+        else:
+            moxa_switch.cli_login()
+        self.active_main_frame.set(1)
+        self.logged_in.set(True)
+        self.config(cursor="")
 
-class Main(ttk.Frame):
-    """Main frame for moxa configurator."""
-
-    def __init__(self, parent):
-        """Initialize the class."""
-        super().__init__(parent)
-        self.place(relx=0.25, y=0, relwidth=0.75, relheight=1)
-        # Ports(self)
-        # SwitchInfo(self)
-        AutoConf(self)
+    def autoconfig(self):
+        """Change frames."""
+        self.active_side_frame.set(1)
+        self.active_main_frame.set(2)
 
 
 class SwitchInfo(ttk.Frame):
@@ -133,6 +162,7 @@ class SwitchInfo(ttk.Frame):
         super().__init__(parent)
         self.create_widgets()
         self.create_layout()
+        self.refresh()
 
     def create_widgets(self):
         """Create the widgets."""
@@ -289,95 +319,112 @@ class Ports(ttk.Frame):
 class AutoConfMenu(ttk.Frame):
     """Auto configuration Frame."""
 
-    swconf = tk.IntVar(value=0)
-    swmainred = tk.IntVar(value=0)
-
-    def __init__(self, parent):
+    def __init__(self, parent, shared_vals) -> None:
         """Initialize the class."""
         super().__init__(parent)
-        self.place(relx=0, rely=0, relwidth=0.25, relheight=1)
+        self.toggle_conf = shared_vals["toggle_conf"]
+        self.toggle_mainred = shared_vals["toggle_mainred"]
+        self.active_main_frame = shared_vals["active_main_frame"]
+        self.active_side_frame = shared_vals["active_side_frame"]
         self.create_widgets()
         self.create_layout()
 
     def create_widgets(self):
         """Create the widgets."""
         self.menuframe = ttk.LabelFrame(self, text="Menu")
-        self.button0 = tk.Button(self.menuframe, text="Return", width=13)
-        self.button1 = tk.Button(self.menuframe, text="Main", width=13)
-        self.button2 = tk.Button(
-            self.menuframe,
-            text="Transfer firmware",
-            width=13,
-        )
+        self.button0 = tk.Button(self.menuframe, text="Return", width=13, command=self.window_control)
+        self.button1 = tk.Button(self.menuframe, text="Show Reserve", width=13, command=self.button1_switch)
+        self.button2 = tk.Button(self.menuframe, text="Show unconfigured", width=13, command=self.button2_switch)
 
     def create_layout(self):
         """Create the layout."""
-        # Create the grid.
-        # self.columnconfigure((0), weight=1, uniform='a')
-        # self.rowconfigure((0,1,2,3,4,5,6,7), weight=1, uniform='a')
-
-        # Place the buttons.
-        self.menuframe.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-        self.done_button = tk.Button(self.menuframe, text="all switches", width=10, command=self.bswitch)
-        self.done_button.pack(side="left")
-        self.main_button = tk.Radiobutton(
-            self.menuframe, text="Main", variable=AutoConfMenu.swmainred, indicatoron=False, value=0, width=8
-        )  # , command=self.refresh)
-        self.main_button.pack(side="left")
-        self.red_button = tk.Radiobutton(
-            self.menuframe, text="Red", variable=AutoConfMenu.swmainred, indicatoron=False, value=1, width=8
-        )  # , command=self.refresh)
-        self.red_button.pack(side="left")
-        self.return_button = tk.Button(self.menuframe, text="Return", width=10)
-        # command=lambda: controller.show_frame(MainPage))
-        self.return_button.pack(side="left")
+        self.menuframe.grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
         self.button0.grid(row=0, column=0, padx=2, pady=2)
         self.button1.grid(row=1, column=0, padx=2, pady=2)
         self.button2.grid(row=2, column=0, padx=2, pady=2)
+        self.pack(expand=True, fill="both")
 
-    def bswitch(self) -> None:
+    def button1_switch(self) -> None:
         """Toggle switch On/Off."""
-        if self.swconf.get() == 0:
-            self.done_button.configure(text="unconfigured")
-            self.swconf.set(1)
+        if self.toggle_mainred.get() == 0:
+            self.button1.configure(text="Show Main")
+            self.toggle_mainred.set(1)
         else:
-            self.done_button.configure(text="all switches")
-            self.swconf.set(0)
-        # AutoConf.refresh()
+            self.button1.configure(text="Show Reserve")
+            self.toggle_mainred.set(0)
+
+    def button2_switch(self) -> None:
+        """Toggle switch On/Off."""
+        if self.toggle_conf.get() == 0:
+            self.button2.configure(text="Show All")
+            self.toggle_conf.set(1)
+        else:
+            self.button2.configure(text="Show unconfigured")
+            self.toggle_conf.set(0)
+
+    def window_control(self) -> None:
+        """Change frames."""
+        self.active_side_frame.set(0)
+        self.active_main_frame.set(0)
 
 
 class AutoConf(ttk.Frame):
     """Devicewindow GUI for moxa configurator."""
 
-    def __init__(self, parent) -> None:
+    def __init__(self, parent, shared_vals) -> None:
         """Initialize the class."""
         super().__init__(parent)
         self.config_file = ConfigFile()
-        self.file = ""
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.frame0 = tk.Frame(self)  # Buttons
-        self.frame0.grid(row=0, column=0, sticky="n")
-        self.frame1 = tk.Frame(self)  # Treeview
-        self.frame1.grid(row=1, column=0, sticky="nsew")
-        self.frame2 = tk.Frame(self)  # Statusline
-        self.frame2.grid(row=2, column=0, sticky="nsew")
-        # Treeview
+        self.file = "/home/stian/Projects/moxaConf/site/test.csv"
+        self.toggle_conf = shared_vals["toggle_conf"]
+        self.toggle_mainred = shared_vals["toggle_mainred"]
+        self.toggle_conf.trace("w", self.refresh)
+        self.toggle_mainred.trace("w", self.refresh)
+        self.header_message = tk.StringVar()
+
+        self.create_frames()
+        self.header()
+        self.treeview_widgets()
+        self.create_layout()
+
+    def create_frames(self) -> None:
+        """Create the frames."""
+        self.header_frame = tk.Frame(self)
+        self.treeview_frame = tk.Frame(self)
+        self.status_frame = tk.Frame(self)
+
+    def create_layout(self) -> None:
+        """Create the layout."""
+        self.header_frame.pack(fill=tk.BOTH)
+        self.treeview_frame.pack(fill=tk.BOTH, expand=True)
+        self.status_frame.pack(fill=tk.BOTH)
+        self.scrollbar.pack(fill=tk.BOTH, side="right")
+        self.tree.pack(fill=tk.BOTH, expand=True)
+        self.pack(fill=tk.BOTH, expand=True, padx=10)
+
+    def header(self) -> None:
+        """Create the header frame."""
+        self.header_message = tk.StringVar()
+        self.header_message.set("test")
+        self.label = tk.Label(self.header_frame, text=self.header_message.get())
+        self.label.pack(fill=tk.BOTH, expand=True)
+
+    def treeview_widgets(self) -> None:
+        """Create the treeview frame."""
         self.columns = ("cab", "ap", "sw_ip", "loc")
-        self.tree = ttk.Treeview(self.frame1, columns=self.columns, show="headings", height=30)
+        self.tree = ttk.Treeview(self.treeview_frame, columns=self.columns, show="headings")
         self.tree.column("cab", width=130, anchor=tk.NW)
-        self.tree.column("ap", width=60, anchor=tk.NW)
-        self.tree.column("sw_ip", width=140, anchor=tk.NW)
-        self.tree.column("loc", width=350, anchor=tk.NW)
         self.tree.heading("cab", text="Cabinet")
+        self.tree.column("ap", width=60, anchor=tk.NW)
         self.tree.heading("ap", text="AP")
+        self.tree.column("sw_ip", width=140, anchor=tk.NW)
         self.tree.heading("sw_ip", text="Switch IP")
+        self.tree.column("loc", width=350, anchor=tk.NW)
         self.tree.heading("loc", text="Location")
         self.tree.bind("<Double-1>", self.item_selected)
-        self.scrollbar = ttk.Scrollbar(self.frame1, orient=tk.VERTICAL, command=self.tree.yview)
-        self.scrollbar.pack(fill=tk.BOTH, expand=True, side="right")
-        self.tree.pack(fill=tk.BOTH, expand=1)
-        # Buttons
+        self.tree.bind("<KeyRelease-Return>", self.item_selected)
+        self.scrollbar = ttk.Scrollbar(self.treeview_frame, orient=tk.VERTICAL, command=self.tree.yview)
+        self.tree.configure(yscrollcommand=self.scrollbar.set)
         self.refresh()
 
     def item_selected(self, event) -> None:
@@ -388,7 +435,7 @@ class AutoConf(ttk.Frame):
         for count, port in enumerate(moxa_switch.get_ifaces()):
             if port == "Up":
                 ports[count] = 1
-        if AutoConfMenu.swmainred.get() == 0:
+        if self.toggle_mainred.get() == 0:
             main_reserve = "M"
         else:
             main_reserve = "R"
@@ -409,25 +456,43 @@ class AutoConf(ttk.Frame):
                 config[0],
                 config[1],
                 moxa_switch.get_sysinfo()[4],
-                True if AutoConfMenu.swmainred.get() == 0 else False,
+                True if self.toggle_mainred.get() == 0 else False,
             )
             self.refresh()
 
-    def refresh(self) -> None:
+    def refresh(self, *args) -> None:
         """Refresh the values in the frame."""
+        _ = args  # Hush some editor warnings
         if self.file == "":
             file = fd.askopenfilename(initialdir="./site/", filetypes=[("Comma Separated files", ".csv")])
             if file != "":
                 self.file = file
 
+        # Clear tree
         for entry in self.tree.get_children():
             self.tree.delete(entry)
-        csvlist = self.read_config(self.file)
-        for entry in csvlist:
+        # Update values in tree
+        for entry in self.read_config(self.file):
             self.tree.insert("", tk.END, values=list(entry))
+        self.tree.selection_set(self.tree.get_children()[0])
+
+        # Configure dynamic header
+        if self.toggle_mainred.get() == 0 and self.toggle_conf.get() == 0:
+            self.header_message.set("Showing all Main Switches")
+            self.label.configure(text=self.header_message.get())
+        elif self.toggle_mainred.get() == 1 and self.toggle_conf.get() == 0:
+            self.header_message.set("Showing all Reserve Switches")
+            self.label.configure(text=self.header_message.get())
+        elif self.toggle_mainred.get() == 0 and self.toggle_conf.get() == 1:
+            self.header_message.set("Showing Main unconfigured Switches")
+            self.label.configure(text=self.header_message.get())
+        elif self.toggle_mainred.get() == 1 and self.toggle_conf.get() == 1:
+            self.header_message.set("Showing Reserve unconfigured Switches")
+            self.label.configure(text=self.header_message.get())
+
         # Statusline
         total_count = len(self.tree.get_children())
-        label = ttk.Label(self.frame2, text=f"Total objects: {total_count}")
+        label = ttk.Label(self.status_frame, text=f"Total objects: {total_count}")
         label.grid(row=0, column=0)
 
     def read_config(self, file: str) -> list:
@@ -440,13 +505,12 @@ class AutoConf(ttk.Frame):
             parsed list (list)
         """
         config = []
-        csvconfig = self.config_file.read_config(file)
-        for row in csvconfig:
+        for row in self.config_file.read_config(file):
             if row["SW"] == "1":
                 # Not configured
-                if AutoConfMenu.swconf.get() == 0:
+                if self.toggle_conf.get() == 0:
                     # Only Main
-                    if AutoConfMenu.swmainred.get() == 0:
+                    if self.toggle_mainred.get() == 0:
                         config.append(
                             (
                                 row["Cabinet"],
@@ -469,7 +533,7 @@ class AutoConf(ttk.Frame):
                 # Configured
                 else:
                     # Only Main
-                    if AutoConfMenu.swmainred.get() == 0:
+                    if self.toggle_mainred.get() == 0:
                         if row["DIPB"] != "" and row["MAC M"] == "":
                             config.append(
                                 (
